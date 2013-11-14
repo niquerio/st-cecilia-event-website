52baac30a13395f632ae2ece9d865b7302eed3e3
Monique Rio <mrio@umich.edu>
2013-11-14 13:05:09 -0500

    updated the disclaimer

--- disclaimer.php
+++ disclaimer.php
@@ -7,14 +7,14 @@ require_once('includes/header.php');
 
 <h1>Disclaimer</h1>
 
-<p>This website is for the Known World Dance Symposiums, held by the
-    <a href="sca.org">Society for Creative Anachronism, Inc.</a> and is maintained by
-    THL Gwommy and THL Darius the Dancer. This site may contain electronic versions of
+<p>This website is for the event St. Cecilia at the Tower, held by the
+    <a href="cynnabar.org">The Barony of Cynnabar</a>, a chapter of <a href="sca.org">Society for Creative Anachronism, Inc.</a> and is maintained by
+    Lady Jadwiga Krzyzanowska. This site may contain electronic versions of
     the event's governing documents. Any discrepancies between the electronic version of
     any information on this site and the printed version that is available from the
     originating office will be decided in favor of the printed version. </p>
 
-<p>Copyright © <?php echo date('Y',time()); ?> by The Known World Dance Symposium <?php echo roman($kwds['KWID']);?>,
+<p>Copyright © <?php echo date('Y',time()); ?> by The Barony of Cynnabar <?php echo roman($kwds['KWID']);?>,
     Society for Creative Anachronism, Inc. The original contributors retain
     the copyright to certain portions of this site. </p>
 
@@ -25,7 +25,7 @@ require_once('includes/header.php');
 
 <p>All external links are not part of The Known World Dance Symposium <?php echo roman($kwds['KWID']);?> website.
     Inclusion of a page or site here is neither implicit nor explicit endorsement of the site.
-    Further, SCA, Inc. is not responsible for content outside of http://www.kwds.org.</p>
+    Further, SCA, Inc. is not responsible for content outside of http://www.cynnabar.org.</p>
 
 
 <?php include_once('includes/footer.php');?>
