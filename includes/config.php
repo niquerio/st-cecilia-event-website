528028f8bdaa44eec9d55fdf6c55b8cb11be7287
Monique Rio <mrio@umich.edu>
2013-11-14 09:25:59 -0500

    put config.php in the gitignore

--- includes/config.php
+++ includes/config.php
@@ -5,13 +5,13 @@
 
 // Set the site's name
 define('SITE_NAME', 'St. Cecilia at the Tower');
-define('SITE_URL', 'http://192.168.0.115/cecilia.cynnabar.org');
+define('SITE_URL', 'http://cynnabar.org/cecilia');
 
 // Database settings
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'cecilia');
 define('DB_USER', 'music');
-define('DB_PASS', 'ilovemusic');
+define('DB_PASS', 'blahblahblah');
 
 /*
 // Set the site's URL
